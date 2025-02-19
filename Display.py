3fb01a0f6c795f4cb695186b21231b48fb569441
SonnyS10 <sonnysalamone@gmail.com>
2025-02-19 14:00:20 -0500

    Co-authored-by: Clynch04 <Clynch04@users.noreply.github.com>

--- Display.py
+++ Display.py
@@ -1,25 +1,17 @@
-import tkinter as tk 
-from tkinter import ttk, Toplevel
 from PIL import Image, ImageTk
 import os 
 
 #Global Variables
+Beer = Image.open(os.path.join(os.path.dirname(__file__), 'Images', 'Beer.jpg'))
+class Display():
+    test_assets = {
+        'Unisensory Neutral Visual': [Beer],
+        'Unisensory Alcohol Visual': [Beer],
+        'MultiSensory Alcohol Visual & Olfactory': [Beer],
+        'MultiSensory Neutral Visual & Olfactory': [Beer],
+        'MultiSensory Alcohol Visual, Tactile & Olfactory' : [Beer],
+        'MultiSensory Neutral Visual, Tactile & Olfactory' : [Beer] 
+    }
 
-#Root Window
-class ImageDisplay:
-    def show_display(self):
-        #Getting the Image path and finding the image
-        image_folder = os.path.join(os.path.dirname(__file__), 'Images')
-        image = 'Beer.jpg'
-        image_path = os.path.join(image_folder, image)
-        image = Image.open(image_path)
-        self.photo = ImageTk.PhotoImage(image)
-        self.image_label = ttk.Label(self, image=self.photo)
-        self.image_label.image = self.photo
-        self.image_label.pack(fill=tk.BOTH)
-
-
-
-
-#root = Display()
-#root.mainloop()
+    
+    
