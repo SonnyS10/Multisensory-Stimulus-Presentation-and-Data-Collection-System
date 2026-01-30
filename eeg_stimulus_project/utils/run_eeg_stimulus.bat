05a5721db3940db14d302311008069b931670950
[Sonny Salamone] <[sonnysalamone@gmail.com]>
2026-01-30 16:42:11 -0500

    Update required Python packages in batch script

--- eeg_stimulus_project/utils/run_eeg_stimulus.bat
+++ eeg_stimulus_project/utils/run_eeg_stimulus.bat
@@ -233,7 +233,7 @@ REM ==============================
 REM Check for all required packages
 REM ==============================
 
-set PACKAGES=PyQt5 pillow numpy matplotlib openpyxl pylsl pyyaml pandas pupil-labs-realtime-api pynput serial pywinauto
+set PACKAGES=PyQt5 pillow numpy matplotlib openpyxl pylsl pyyaml pandas pupil-labs-realtime-api pynput serial pywinauto paramiko
 
 echo Checking for required Python packages...
 for %%P in (%PACKAGES%) do (
